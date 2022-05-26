60b090dd61d34fd9565bba08dad5e63872dc5e8d
im ranjitha <ranjithatk101@gmail.com>
2022-05-27 01:30:09 +0530

    added mock qstn

--- reduce.js
+++ reduce.js
@@ -4,3 +4,25 @@ let output = array.reduce((acc, curr) => {
   return acc
 }, 0)
 console.log(output)
+
+const out = array.reduce((max, curr) => {
+  if (max < curr) {
+    max = curr
+  }
+  return max
+}, 0)
+console.log(out)
+
+let detail = [
+  { name: "ranji", age: 24 },
+  { name: "buddy", age: 28 },
+]
+let sumOfAge = detail.reduce((acc, curr) => {
+  return (acc += curr.age)
+}, 0)
+console.log(sumOfAge)
+
+const sum = detail.reduce((acc, curr) => {
+  return (acc += curr.age)
+}, 0)
+console.log(sum)
