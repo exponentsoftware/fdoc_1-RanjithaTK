fd86244bf309ad832b36a941c8f5c1d16640c50a
im ranjitha <ranjithatk101@gmail.com>
2022-05-26 17:33:06 +0530

    changes

--- hoc.js
+++ hoc.js
@@ -1,8 +1,11 @@
 let array = [1, 2, 3, 4, 5, 6]
-array = array.map((arr) => arr * 2)
+multiplyByTwo = array.map((arr) => arr * 2)
 
-console.log(array)
+console.log(multiplyByTwo)
 //to convert to binary
 
 array2 = array.map((arr) => arr.toString(2))
 console.log(array2)
+//to get odd
+odd = array.filter((arr) => arr % 2)
+console.log(odd)
